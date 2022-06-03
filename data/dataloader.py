676fe1862e0689728e884d8fc6f5f74b216c76db
Md. Saiful Bari Siddiqui <iftu2345@gmail.com>
2022-06-04 04:26:44 +0600

    Update dataloader.py

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,1 +1,24 @@
+train_transforms = torchvision.transforms.Compose(
+    [
+        torchvision.transforms.RandomCrop(32, padding=4),
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.ToTensor(),
+        cifar10_normalization(),
+    ]
+)
 
+test_transforms = torchvision.transforms.Compose(
+    [
+        torchvision.transforms.ToTensor(),
+        cifar10_normalization(),
+    ]
+)
+
+cifar10_dm = CIFAR10DataModule(
+    data_dir=cfg[1]['exp_params']['PATH_DATASETS'],
+    batch_size=cfg[1]['exp_params']['BATCH_SIZE'],
+    num_workers=cfg[1]['exp_params']['NUM_WORKERS'],
+    train_transforms=train_transforms,
+    test_transforms=test_transforms,
+    val_transforms=test_transforms,
+)
